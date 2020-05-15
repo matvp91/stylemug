41bc7b73e39b00e40a7d621d43800d3a11ba348b
a <matvp91@gmail.com>
2020-05-15 16:53:31 +0200

    Added CI typing checks.

--- packages/stylemug-compiler/src/compile.js
+++ packages/stylemug-compiler/src/compile.js
@@ -1,13 +1,28 @@
+// @flow
+
 import fnv1a from 'fnv1a';
 import { save as extractorSave } from './extractor';
+import type {
+  SchemaDefs,
+  SchemaDefSelectors,
+  RuleEntry,
+  CompiledSchemaDefs,
+} from './types';
 
-function hash(str) {
+function hash(str: string) {
   let hash = fnv1a(str).toString(16);
-  const prepend = str.match(/[a-zA-Z]/).pop() || 'x';
+  const match = str.match(/[a-zA-Z]/);
+  const prepend = (match ? match.pop() : undefined) || 'x';
   return `${prepend}${hash}`;
 }
 
-export function compileSelectors(selectors, children, media) {
+export function compileSelectors(
+  selectors: SchemaDefSelectors,
+  children: string,
+  media: ?string
+): {
+  [className: string]: RuleEntry,
+} {
   let result = {};
 
   for (let key in selectors) {
@@ -34,13 +49,14 @@ export function compileSelectors(selectors, children, media) {
 
       case 'number':
       case 'string':
-        const className = hash(key + value + children + (media || ''));
+        const mediaKey = media || '';
+        const className = hash(key + value + children + mediaKey);
 
-        const entry = {
+        const entry: RuleEntry = {
           // Provide a key id to match selectors setting the same key,
           // for example, when for a given key, two classes apply with both
           // backgroundColor as propery, we can filter out the first one by specificity.
-          keyId: hash(key + children + media),
+          keyId: hash(key + children + mediaKey),
           key,
           value,
           children,
@@ -57,7 +73,7 @@ export function compileSelectors(selectors, children, media) {
   return result;
 }
 
-export function compileSchema(schema) {
+export function compileSchema(schema: SchemaDefs): CompiledSchemaDefs {
   const result = {};
 
   for (let key in schema) {
--- packages/stylemug-compiler/src/index.js
+++ packages/stylemug-compiler/src/index.js
@@ -2,8 +2,4 @@ export { compileSchema } from './compile';
 
 export { flushAsCss } from './extractor';
 
-export type SchemaMap = {|
-  [className: string]: {|
-    [keyId: string]: string,
-  |},
-|};
+export type { SchemaMap } from './types';
