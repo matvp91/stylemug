e09c7882950848f6215018f7483d13aa3d6bc474
a <matvp91@gmail.com>
2020-05-18 22:06:42 +0200

    Fixes #11

---
+++ example/src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import stylemug from 'stylemug';
+import { globalStyles } from './globals';
+
+const styles = stylemug.create({
+  title: {
+    fontSize: '31px',
+    fontFamily: 'courier',
+    color: '#444',
+  },
+  titleRed: {
+    color: 'red',
+  },
+});
+
+export default function App() {
+  const [show, setShow] = useState(false);
+
+  function onClick() {
+    setShow(!show);
+  }
+
+  return (
+    <div className={styles(globalStyles.container)}>
+      <h1 className={styles('title', show && 'titleRed')}>Hello World</h1>
+      <button onClick={onClick}>Click</button>
+    </div>
+  );
+}
